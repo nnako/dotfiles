e3f8436839a17d8000fd24a80c4f373e38951d2f
nnako <nnako@web.de>
2016-04-02 22:04:19 +0200

    separated config, hardware and apt installation into different init files

---
+++ init/15_raspbian_config.sh
@@ -0,0 +1,21 @@
+#
+## check for execution permission
+#
+
+is_raspbian || return 1
+
+
+
+
+#
+## install hardware relevant stuff
+#
+
+e_header "Configuring HARDWARE (keyboard, timezone,...)"
+
+# keyboard, display and timezone
+sudo dpkg-reconfigure locales
+sudo dpkg-reconfigure keyboard-configuration
+sudo dpkg-reconfigure tzdata
+sudo apt-get -qq install rpi-update
+sudo rpi-update
