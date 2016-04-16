bac0a3ea8692c8c52a93509ce4ba6af0c9eed937
nnako <nnako@web.de>
2016-04-16 14:52:50 +0200

    FIX: removed debugging hold points from script

--- init/50_python.sh
+++ init/50_python.sh
@@ -100,14 +100,8 @@ e_header "Installing Virtual Environments"
 sudo pip -qq install virtualenv
 sudo pip -qq install virtualenvwrapper
 
-# PAUSE
-read -p "PRESS <ENTER> to resume (FIRST POINT)"
-
 # update configurations for current console
 source ~/.profile
-
-# PAUSE
-read -p "PRESS <ENTER> to resume (SECOND POINT)"
 
 # ENV for python v2 and update pip
 if [[ ! -d $HOME/.virtualenvs/${sPython2venv} ]]; then
