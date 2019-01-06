7925751cca9ea38bad32abfbd9f494cfdd0b233e
nnako <nnako@web.de>
2019-01-06 13:45:48 +0100

    NEW: added installation of PHP7

---
+++ init/30_PHP7.sh
@@ -0,0 +1,46 @@
+#
+## check for execution permission
+#
+
+is_raspbian || return 1
+
+
+
+
+#
+## install necessary packages
+#
+
+# define packages
+packages=(
+  php7.0
+  php7.0-cli
+  php7.0-fpm
+  php7.0-gd
+  php7.0-curl
+  php7.0-xml
+  php7.0-zip
+  php7.0-opcache
+  php7.0-mbstring
+)
+
+# filter packages which have already been installed
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+# install using apt-get
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages\n${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
+
+
+
+
+#
+## set some configutations
+#
+
+# start NGINX service
+#...
