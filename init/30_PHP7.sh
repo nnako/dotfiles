c18bae676fe9a8f81b05506ab2fd1604339e8a1d
nnako <nnako@web.de>
2019-01-06 22:23:05 +0100

    MOD: do some configurations on NGINX service after adding PHP

--- init/30_PHP7.sh
+++ init/30_PHP7.sh
@@ -42,5 +42,27 @@ fi
 ## set some configutations
 #
 
-# start NGINX service
-#...
+# create backup of default site
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-do-not-touch
+
+# add index.php to default index files
+sudo sed -i "s/index index.html/index index.php index.html;/g" /etc/nginx/sites-available/default
+
+# uncomment fast cgi blocks
+sudo sed -i "s/#location ~ \\\.php\\$/location ~ \\\.phpi\\$/g" /etc/nginx/sites-available/default
+sudo sed -i "s/#\sfastcgi_pass unix/fastcgi_pass unix/g" /etc/nginx/sites-available/default
+
+# add fast cgi path info
+sudo sed -i "/#location ~ \\\.php\\$/a fastcgi_split_path_info ^(.+\\\.php)(\/.+)\\$;" /etc/nginx/sites-available/default
+sudo sed -i "/fastcgi_pass unix/a fastcgi_index index.php;" /etc/nginx/sites-available/default
+sudo sed -i "/fastcgi_pass unix/a include fastcgi.conf;" /etc/nginx/sites-available/default
+
+
+
+
+#
+# restart
+#
+
+sudo /etc/init.d/nginx restart
+sudo /etc/init.d/php7.0-fpm restart
