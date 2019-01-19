6faa2c622b831ed46c802a0a5ecf93f42c1584e7
nnako <nnako@web.de>
2019-01-19 20:05:49 +0100

    NEW: installation script for MARIADB database

---
+++ init/32_MARIADB.sh
@@ -0,0 +1,39 @@
+#
+## check for execution permission
+#
+
+is_raspbian || return 1
+
+
+
+
+#
+## install necessary packages
+#
+
+# define packages
+packages=(
+  mariadb-server
+  mariadb-client
+)
+
+# filter packages which have already been installed
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+# install using apt-get
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages\n${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
+
+
+
+
+#
+## set some configutations
+#
+
+# secure the database
+sudo mysql_secure_installation
