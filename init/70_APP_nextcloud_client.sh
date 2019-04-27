06f2297fdaeb484c3d0b9265791a8641f69448eb
nnako <nnako@web.de>
2019-04-27 14:54:12 +0200

    NEW: nextcloud client installation as an application

---
+++ init/70_APP_nextcloud_client.sh
@@ -0,0 +1,53 @@
+#
+# settings
+#
+
+strAppName=APP__nextcloud_client
+
+
+
+
+#
+# get required packages
+#
+
+# define packages
+packages=(
+  git
+  libsqlite3-dev
+  qt5-default
+  libqt5webkit5-dev
+  qt5keychain-dev
+  cmake
+  build-essential
+  libowncloudsync0
+  libssl1.0-dev
+)
+
+# filter packages which have already been installed
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+# install using apt-get
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages\n${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
+
+
+
+
+#
+# get and build the application
+#
+
+git clone https://github.com/nextcloud/client_theming.git $strAppName
+cd $strAppName
+git submodule update --init --recursive
+mkdir build-linux
+cd build-linux
+cmake -D OEM_THEME_DIR=$(realpath ../nextcloudtheme) -DCMAKE_INSTALL_PREFIX=/usr  ../client
+make
+sudo make install
+
