e870818c94e8b8a3a468044d165d296efc15f6c2
nnako <nnako@web.de>
2019-07-09 13:08:55 +0200

    added some commandline scripts to start scans

--- init/70_APP_HPLIP_CUPS.sh
+++ init/70_APP_HPLIP_CUPS.sh
@@ -3,7 +3,9 @@
 
 
 
-# Installing HPLIP for CUPS on a Raspberry Pi
+#
+# install CUPS together wirh HPLIP for HP printers
+#
 
 # If you have a HP printer or scanner and you want to be able to use it on your
 # Raspberryi Pi running the Raspbian operating system, you`ll have to install
@@ -19,7 +21,9 @@ sudo usermod -a -G lpadmin pi
 
 
 
-# If you also would like to use your MFP/scanner
+#
+# install SANE scanner control software
+#
 
 sudo apt-get install sane
 sudo sane-find-scanner
@@ -27,4 +31,67 @@ sudo sane-find-scanner
 
 
 
-# The configuration and applications are up to you, the requirements are now installed.
+#
+# create some scanner scripts
+#
+
+# ADF A4 BW 200dpi SS
+FILEPATH=/home/pi/scan_feed_A4_200dpi_bw_ss.sh
+cat > ${FILEPATH} <<EOF
+# scan original
+scanadf --source=ADF --mode=Lineart --resolution=200 -x 210 -y 295 -o /home/pi/scan_%02d.pnm
+
+# convert to PNG
+convert scan_*.pnm scan_*.png
+rm scan_*.pnm
+
+# convert to PDF
+convert scan_*.png scan.pdf
+rm scan_*.png
+
+# move into nextcloud
+mv scan.pdf ~/NEXTCLOUD/_scan/
+
+EOF
+sudo chmod a+x ${FILEPATH}
+
+# FLAT A4 BW 200dpi
+FILEPATH=/home/pi/scan_flat_A4_200dpi_bw.sh
+cat > ${FILEPATH} <<EOF
+# scan original
+scanimage --mode=Lineart --resolution=200 -x 210 -y 295 >/home/pi/scan.pnm
+
+# convert to PNG
+convert scan.pnm scan.png
+rm scan.pnm
+
+# convert to PDF
+convert scan.png scan.pdf
+rm scan.png
+
+# move into nextcloud
+mv scan.pdf ~/NEXTCLOUD/_scan/
+
+EOF
+sudo chmod a+x ${FILEPATH}
+
+# FLAT A5 BW 200dpi
+FILEPATH=/home/pi/scan_flat_A5_200dpi_bw.sh
+cat > ${FILEPATH} <<EOF
+# scan original
+scanimage --mode=Lineart --resolution=200 -x 148 -y 210 >/home/pi/scan.pnm
+
+# convert to PNG
+convert scan.pnm scan.png
+rm scan.pnm
+
+# convert to PDF
+convert scan.png scan.pdf
+rm scan.png
+
+# move into nextcloud
+mv scan.pdf ~/NEXTCLOUD/_scan/
+
+EOF
+sudo chmod a+x ${FILEPATH}
+
