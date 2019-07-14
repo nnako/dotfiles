c513f7a738521fff4d963a338875cc90e4f6941c
nnako <nnako@web.de>
2019-07-14 22:10:08 +0200

    NEW: possibility to specify number of pages and file name for flatbed scan A4 and A5

--- init/70_APP_HPLIP_CUPS.sh
+++ init/70_APP_HPLIP_CUPS.sh
@@ -58,40 +58,125 @@ sudo chmod a+x ${FILEPATH}
 # FLAT A4 BW 200dpi
 FILEPATH=/home/pi/scan_flat_A4_200dpi_bw.sh
 cat > ${FILEPATH} <<EOF
-# scan original
-scanimage --mode=Lineart --resolution=200 -x 210 -y 295 >/home/pi/scan.pnm
+#
+# get user's wish of document name
+#
 
-# convert to PNG
-convert scan.pnm scan.png
-rm scan.pnm
+if [[ $# -ge 2 ]]; then
+    filename=$2
+else
+    filename=scan
+fi
 
-# convert to PDF
-convert scan.png scan.pdf
-rm scan.png
 
-# move into nextcloud
-mv scan.pdf ~/NEXTCLOUD/_scan/
 
+
+#
+# get user's wished number of pages
+#
+
+if [[ $# -ge 1 ]]; then
+
+    for (( i=1; i<=$1; i++ )); do
+
+        # wait for user to place the document
+        read -p "PLEASE PUT PAGE#$i ONTO THE FLATBED" -n1
+
+        # scan original
+        scanimage --mode=Lineart --resolution=200 -x 210 -y 295 >/home/pi/scan_$i.pnm
+
+    done
+
+    # convert to PNG
+    convert scan_*.pnm scan_*.png
+    rm scan_*.pnm
+
+    # convert to PDF
+    convert scan_*.png $filename.pdf
+    rm scan_*.png
+
+    # move into nextcloud
+    mv $filename.pdf ~/NEXTCLOUD/_scan/
+
+else
+
+    # scan original
+    scanimage --mode=Lineart --resolution=200 -x 210 -y 295 >/home/pi/scan.pnm
+
+    # convert to PNG
+    convert scan.pnm scan.png
+    rm scan.pnm
+
+    # convert to PDF
+    convert scan.png $filename.pdf
+    rm scan.png
+
+    # move into nextcloud
+    mv $filename.pdf ~/NEXTCLOUD/_scan/
+fi
 EOF
 sudo chmod a+x ${FILEPATH}
 
 # FLAT A5 BW 200dpi
 FILEPATH=/home/pi/scan_flat_A5_200dpi_bw.sh
 cat > ${FILEPATH} <<EOF
-# scan original
-scanimage --mode=Lineart --resolution=200 -x 148 -y 210 >/home/pi/scan.pnm
+#
+# get user's wish of document name
+#
 
-# convert to PNG
-convert scan.pnm scan.png
-rm scan.pnm
+if [[ $# -ge 2 ]]; then
+    filename=$2
+else
+    filename=scan
+fi
 
-# convert to PDF
-convert scan.png scan.pdf
-rm scan.png
 
-# move into nextcloud
-mv scan.pdf ~/NEXTCLOUD/_scan/
 
+
+#
+# get user's wish of number of pages
+#
+
+if [[ $# -ge 1 ]]; then
+
+    for (( i=1; i<=$1; i++ )); do
+
+        # wait for user to place the document
+        read -p "PLEASE PUT PAGE#$i ONTO THE FLATBED" -n1
+
+        # scan original
+        scanimage --mode=Lineart --resolution=200 -x 148 -y 210 >/home/pi/scan_$i.pnm
+
+    done
+
+    # convert to PNG
+    convert scan_*.pnm scan_*.png
+    rm scan_*.pnm
+
+    # convert to PDF
+    convert scan_*.png $filename.pdf
+    rm scan_*.png
+
+    # move into nextcloud
+    mv $filename.pdf ~/NEXTCLOUD/_scan/
+
+else
+
+    # scan original
+    scanimage --mode=Lineart --resolution=200 -x 148 -y 210 >/home/pi/scan.pnm
+
+    # convert to PNG
+    convert scan.pnm scan.png
+    rm scan.pnm
+
+    # convert to PDF
+    convert scan.png $filename.pdf
+    rm scan.png
+
+    # move into nextcloud
+    mv $filename.pdf ~/NEXTCLOUD/_scan/
+
+fi
 EOF
 sudo chmod a+x ${FILEPATH}
 
