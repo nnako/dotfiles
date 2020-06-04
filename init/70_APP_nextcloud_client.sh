6a1cb0b1e90ebf43d6a15e76311947a711b5b0a7
nnako <nnako@web.de>
2020-06-04 17:59:05 +0200

    FIX: finally got it right from https://crycode.de/nextcloud-client-auf-dem-raspberry-pi

--- init/70_APP_nextcloud_client.sh
+++ init/70_APP_nextcloud_client.sh
@@ -13,26 +13,25 @@ strAppName=APP__nextcloud_client
 
 # define packages
 packages=(
+  build-essential
   git
-  libsqlite3-dev
-  qt5-default
+  cmake
   openssl
   libssl-dev
-  libzip-dev
-  qtdeclarative5-dev
-  qtwebengine5-dev
-  qttools5-dev
   sqlite3
   libsqlite3-dev
-  libqt5svg5-dev
-  zlib1g-dev
+  qt5-default
   libqt5webkit5-dev
+  qttools5-dev
+  qttools5-dev-tools
+  python-sphinx
+  texlive-latex-base
+  inotify-tools
   qt5keychain-dev
-  cmake
-  build-essential
-  libowncloudsync0
+  doxygen
+  extra-cmake-modules
+  kio-dev
 )
-  #libssl1.0-dev
 
 # filter packages which have already been installed
 packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
@@ -52,19 +51,25 @@ fi
 # get and build the application
 #
 
-#git clone https://github.com/nextcloud/client_theming.git $strAppName
-#cd $strAppName
-#git submodule update --init --recursive
-#mkdir build-linux
-#cd build-linux
-#cmake -D OEM_THEME_DIR=$(realpath ../nextcloudtheme) -DCMAKE_INSTALL_PREFIX=/usr  ../client
-#make
-#sudo make install
-
-# get and install from new location
-git clone https://github.com/nextcloud/desktop.git ${strAppName}_SOURCE
-cd ${strAppName}_SOURCE
+git clone https://github.com/nextcloud/client_theming.git $strAppName
+cd $strAppName
+git submodule update --init
+cd client
+git submodule update --init
+cd ..
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=~/$strAppName -DCMAKE_BUILD_TYPE=Debug -DNO_SHIBBOLETH=1
-make install
+cmake -D OEM_THEME_DIR=$(pwd)/../nextcloudtheme ../client
+sed -i 's/Icon=nextcloud/Icon=Nextcloud/g' src/gui/nextcloud.desktop
+sed -i 's/Icon\[\(.*\)\]=nextcloud/Icon\[/1/]=Nextcloud/g' src/gui/nextcloud.desktop
+
+# takes some minutes to compile
+make
+
+sudo make install
+
+echo 'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/arm-libux-gnueabihf' | sudo tee -a /etc/environment
+grep '/usr/local/lib/arm-linux-gnueabihf' /etc/ld.so.conf.d/arm-linux-gnueabihf.conf >/dev/null 2>&1 || echo '/usr/local/lib/arm-linux-gnueabihf' | sudo tee -a /etc/ld.so.conf.d/arm-linux-gnueabihf.conf
+grep '/usr/local/lib/arm-linux-gnueabihf' /etc/ld.so.conf.d/x86_64-linux-gnu.conf >/dev/null 2>&1 || echo '/usr/local/lib/arm-linux-gnueabihf' | sudo tee -a /etc/ld.so.conf.d/x86_64-linux-gnu.conf
+sudo ldconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/arm-linux-gnueabihf
