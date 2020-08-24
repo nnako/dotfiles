81848b992a80ac291ba5bb74f4b5dac997b9ab74
Kohn, Dr. Peter Nnamdi (TM-D) <peter.nnamdi.kohn@iav.de>
2020-08-24 20:01:11 +0200

    FIX: some structure changes and install necessary packages

--- init/70_APP_surveillance.sh
+++ init/70_APP_surveillance.sh
@@ -11,6 +11,43 @@ prjpath="https://bitbucket.com/nnako/surveillance.git"
 
 
 #
+# get and install relevant packages (if not already present)
+#
+
+# used applications
+#  -> picamera   Application and Python module for camera access
+#  -> pillow     Python module for image manipulation
+#  -> sendemail  CLI Application for transmission of emails
+
+# define packages
+packages=(
+    python-picamera
+    python3-picamera
+    libjpeg-dev
+    libjpeg62-dev
+    zlib1g-dev
+    libfreetype6-dev
+    liblcms1-dev
+    sendemail
+    libio-socket-ssl-perl
+    libnet-ssleay-perl
+)
+
+# filter packages which have already been installed
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+# install using apt-get
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages\n${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
+
+
+
+
+#
 # handle virtual environment for project
 #
 
@@ -48,61 +85,14 @@ fi
 
 
 #
-# get and install relevant packages (if not already present)
-#
-
-# used applications
-#  -> picamera   Application and Python module for camera access
-#  -> pillow     Python module for image manipulation
-#  -> sendemail  CLI Application for transmission of emails
-
-# define packages
-packages=(
-    python-picamera
-    python3-picamera
-    libjpeg-dev
-    libjpeg62-dev
-    zlib1g-dev
-    libfreetype6-dev
-    liblcms1-dev
-    sendemail
-    libio-socket-ssl-perl
-    libnet-ssleay-perl
-)
-
-# filter packages which have already been installed
-packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
-
-# install using apt-get
-if (( ${#packages[@]} > 0 )); then
-  e_header "Installing APT packages\n${packages[*]}"
-  for package in "${packages[@]}"; do
-    sudo apt-get -qq install "$package"
-  done
-fi
-
-
-
-
-
-#
 # install PIL
 #
 
-python3 -m pip install pillow
+# we are inside the virtual environment, here
 
+pip install pillow
+pip install picamera
 
-
-
-#
-# configure web service
-#
-
-#...
-
-
-
-
-# deactivate virtual environment
-#deactivate
+# clean exit from virtual environment
+deactivate
 
