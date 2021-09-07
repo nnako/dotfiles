cbc826935049db32c7fe623aef02a14dfda097a0
Kohn, Dr. Peter Nnamdi (TM-D) <peter.nnamdi.kohn@iav.de>
2021-09-07 22:56:30 +0200

    FIX: installer for gateway project to be up-to-date

--- init/70_APP_gateway.sh
+++ init/70_APP_gateway.sh
@@ -1,24 +1,47 @@
 #
-## set source code path settings
+# set source code path settings
 #
 
+# project
 prjname="APP__gateway"
 prjpath="pi@192.168.1.49:/home/pi/usbdrv/_python/$prjname.git"
+
+# MAILBOT
 libname_mailbot="LIB__mailbot"
 libpath_mailbot="pi@192.168.1.49:/home/pi/usbdrv/_python/$libname_mailbot.git"
-libname_freeplane="LIB__freeplane_api"
+
+# FREEPLANE
+libname_freeplane="LIB__freeplane"
 libpath_freeplane="pi@192.168.1.49:/home/pi/usbdrv/_python/$libname_freeplane.git"
+
+# DROPBOX
 appname_dropbox="Dropbox-Uploader"
+appname_dropbox_local="dropbox_updater"
 apppath_dropbox="https://github.com/andreafabrizi/$appname_dropbox.git"
 
-# move into Python v2 virtual environment
-workon $sPython2venv
+# DEV ENV
+envname="ENV__gateway__py3"
 
 
 
 
 #
-## get and prepare project folders
+# create and activate virtual environment
+#
+
+# get create python3 virtual environment (if not already present)
+if [[ ! -d "$HOME/$envname" ]]; then
+    python3 -m venv $HOME/$envname
+fi
+
+# activate
+source /home/pi/$envname/bin/activate
+
+
+
+
+#
+# get and prepare project folders
 #
 
 # get project source code (if not already present)
@@ -30,44 +53,66 @@ fi
 cd $HOME/$prjname
 
 # prepare executable application (neccessity of click module)
-#pip install --editable .
+pip install --editable .
 
 # create temporary project folder (if not existent)
 if [[ ! -d "$HOME/.$prjname" ]]; then
     mkdir $HOME/.$prjname
 fi
 
-
-
-
-#
-## get relevant packages (if not already present)
-#
-
-# email processing
-sudo apt-get -qq install libffi-dev   # required for mailbot module
-sudo apt-get -qq install sendemail    # required for email attachments
-sudo apt-get -qq install libio-socket-ssl-perl  # required for sendemail on RPi
-sudo apt-get -qq install libnet-ssleay-perl     # required for sendemail on RPi
-
-# audio processing
-sudo apt-get -qq install alsa         # required for sound output
-sudo apt-get -qq install mplayer      # required for sound replay
-sudo apt-get -qq install festival     # festival speech package and application
-sudo apt-get -qq install espeak       # espeak speech package and application
-
-# image processing
-sudo apt-get -qq install imagemagick  # for operations on bitmap graphics
-sudo apt-get -qq install pdftk        # for operations on PDF files
-
-# automation of console
-#sudo apt-get -qq install expect       # for automation of interactive console
+cd ..
 
 
 
 
 #
-## get and install relevant python modules from source
+# get relevant packages (if not already present)
+#
+
+## email processing
+#sudo apt -qq install libffi-dev             # required for mailbot module
+#sudo apt -qq install sendemail              # required for email attachments
+#sudo apt -qq install libio-socket-ssl-perl  # required for sendemail on RPi
+#sudo apt -qq install libnet-ssleay-perl     # required for sendemail on RPi
+
+## audio processing
+#sudo apt -qq install alsa                   # required for sound output
+#sudo apt -qq install mplayer                # required for sound replay
+#sudo apt -qq install festival               # festival speech package and application
+#sudo apt -qq install espeak                 # espeak speech package and application
+
+## image processing
+#sudo apt -qq install imagemagick            # for operations on bitmap graphics
+#sudo apt -qq install pdftk                  # for operations on PDF files
+
+# define packages
+packages=(
+  libffi-dev
+  sendemail
+  libio-socket-ssl-perl
+  libnet-ssleay-perl
+  imagemagick
+  pdftk
+)
+
+# filter packages which have already been installed
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+# install using apt
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing SYSTEM PACKAGES and APPLICATIONS\n${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt -y -qq install "$package"
+  done
+fi
+
+
+
+
+#
+# get and install relevant python modules from source
+#
+
 # other modules are handled from within the project's setup.py
 
 pip install pyzmail
@@ -76,12 +121,14 @@ pip install argparse
 pip install configparser
 pip install markdown
 pip install pexpect
+pip install html2text
+pip install lxml
 
 
 
 
 #
-## get mailbot plugin from source
+# get mailbot plugin from source
 #
 
 # get source (only if directory was not present)
@@ -89,15 +136,17 @@ if [[ ! -d "$HOME/$libname_mailbot" ]]; then
     git clone $libpath_mailbot $HOME/$libname_mailbot
 fi
 
-# build mailbot into python packages
+# install local mailbot package (editable)
 cd $HOME/$libname_mailbot
-python setup.py install
+#python setup.py install
+pip install -e .
+cd ..
 
 
 
 
 #
-## get freeplane api
+# get freeplane api
 #
 
 # get source (only if directory was not present)
@@ -106,7 +155,7 @@ if [[ ! -d "$HOME/$libname_freeplane" ]]; then
 fi
 
 # get dependencies
-pip install html2text
+#pip install html2text
 
 # build freeplane api into python packages
 cd $HOME/$libname_freeplane
@@ -116,12 +165,12 @@ python setup.py install
 
 
 #
-## get relevant applications
+# get relevant applications
 #
 
 # get Dropbox-Uploader source (only if directory was not present)
-if [[ ! -d "$HOME/APP__dropbox_uploader" ]]; then
-    git clone $apppath_dropbox $HOME/APP__dropbox_uploader
+if [[ ! -d "$HOME/$appname_dropbox_local" ]]; then
+    git clone $apppath_dropbox $HOME/$appname_dropbox_local
 fi
 
 
